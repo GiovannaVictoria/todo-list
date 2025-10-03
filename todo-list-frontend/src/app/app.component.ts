02100df1e4e559646094cb3c5d6f1586447fda21
GiovannaVictoria <giovannavictoriarossetto@gmail.com>
2025-10-03 18:07:36 -0300

    first task: show the loading bar only while data as loading

--- todo-list-frontend/src/app/app.component.ts
+++ todo-list-frontend/src/app/app.component.ts
@@ -1,6 +1,6 @@
 import {Component} from '@angular/core';
 import {Todo, TodoService} from "./todo.service";
-import {Observable} from "rxjs";
+import {Observable, Subscription} from "rxjs";
 
 @Component({
   selector: 'app-root',
@@ -13,7 +13,7 @@ import {Observable} from "rxjs";
     <div class="list">
       <label for="search">Search...</label>
       <input id="search" type="text">
-      <app-progress-bar></app-progress-bar>
+      <app-progress-bar *ngIf="isLoading"></app-progress-bar>
       <app-todo-item *ngFor="let todo of todos$ | async" [item]="todo"></app-todo-item>
     </div>
   `,
@@ -22,8 +22,17 @@ import {Observable} from "rxjs";
 export class AppComponent {
 
   readonly todos$: Observable<Todo[]>;
+  isLoading = true;
+  subscription: Subscription;
 
   constructor(todoService: TodoService) {
     this.todos$ = todoService.getAll();
+    this.subscription = this.todos$.subscribe({
+      complete: () => this.isLoading = false
+    });
+  }
+
+  ngOnDestroy() {
+    this.subscription.unsubscribe()
   }
 }
